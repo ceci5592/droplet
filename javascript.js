b3eca12dcb9c582f366cefd1f91efcaad125dd0a
rebe4175 <kea071096#>
2018-12-20 11:47:38 +0100

    final version

--- javascript.js
+++ javascript.js
@@ -6,9 +6,11 @@ let postTemplate = document.querySelector("[data-template]");
 let postContainer = document.querySelector(".container");
 let kategoriFilter = "alle";
 let dest = document.querySelector(".data-container");
+let basketNumber = 0;
+let cartnumber = document.querySelector("#cart-number");
 
 document.querySelectorAll(".product-item").forEach(knap => {
-    knap.addEventListener("click", filtrering)
+    knap.addEventListener("click", filtrering);
 });
 
 let banner = [];
@@ -17,56 +19,59 @@ let banner_1 = document.querySelector(".banner_1");
 let banner_2 = document.querySelector(".banner_2");
 let banner_3 = document.querySelector(".banner_3");
 
+let template_id = document.querySelector("#template_id");
 
 function init() {
-
     getJSON();
     banner_1_func();
 }
 
 function filtrering() {
-
-    console.log("filter apply");
-
     kategoriFilter = this.getAttribute("data-kategori");
-
-    console.log(kategoriFilter);
 
     visProducts();
 }
 
-
 async function getJSON() {
-
-
-    let myJson = await fetch("https://www.rebeccamortensen.dk/kea/lasaite/wp-json/wp/v2/products?_embed&per_page=100");
+    let myJson = await fetch(
+        "https://www.rebeccamortensen.dk/kea/lasaite/wp-json/wp/v2/products?_embed&per_page=100"
+    );
     produkter = await myJson.json();
 
     visProducts();
-
 }
 
 function visProducts() {
-
     postContainer.innerHTML = "";
 
-
     produkter.forEach(produkt => {
-
         if (produkt.acf.type == kategoriFilter || kategoriFilter == "alle") {
-
-            console.log(produkt);
-
-
             let klon = postTemplate.cloneNode(true).content;
 
-            klon.querySelector("article").addEventListener("click", () => {
-                window.location.href = "singleview.html?id=" + produkt.id;
+            klon.querySelector("article").addEventListener("click", e => {
+                e.preventDefault();
 
-                console.log("klik på produkt");
+                console.log(e);
+
+                if (e.target.localName != "button") {
+                    window.location.href = "singleview.html?id=" + produkt.id;
+
+                }
 
             });
 
+            // Basket kode
+            klon
+                .querySelector("article [data-id=template_id]")
+                .addEventListener("click", e => {
+                    e.preventDefault();
+                    basketNumber++;
+                    document.querySelector("#cart-number").textContent = basketNumber;
+                    document.querySelector("#cart").href += "id=" + produkt.id + "&";
+                    document.querySelector;
+                    cartnumber.style.display = "flex";
+
+                });
 
             klon.querySelector("[data-price]").textContent = produkt.acf.price;
             klon.querySelector("[data-title]").textContent = produkt.acf.name;
@@ -74,13 +79,9 @@ function visProducts() {
             klon.querySelector("[data-img]").setAttribute("src", produkt.acf.image);
 
             postContainer.appendChild(klon);
-
         }
-
     });
-
 }
-
 
 function myDropMenu() {
     document.getElementById("myDropdown").classList.toggle("show");
@@ -89,59 +90,44 @@ function myDropMenu() {
 // Close the dropdown menu if the user clicks outside of it
 
 window.onclick = function (event) {
-    if (!event.target.matches('.dropbtn')) {
-
+    if (!event.target.matches(".dropbtn")) {
         let dropdowns = document.getElementsByClassName("dropdown-content");
         let i;
         for (i = 0; i < dropdowns.length; i++) {
             let openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
+            if (openDropdown.classList.contains("show")) {
+                openDropdown.classList.remove("show");
             }
         }
     }
-}
-
+};
 
 function filterToggle() {
-
     var x = document.getElementById("content-drop");
     if (x.style.display === "block") {
         x.style.display = "none";
     } else {
         x.style.display = "block";
     }
-
 }
 
 function banner_1_func() {
-
-    console.log("banner 1");
-
     banner_1.style.display = "block";
     banner_2.style.display = "none";
     banner_3.style.display = "none";
 
     setTimeout(banner_2_func, 5000);
-
 }
 
 function banner_2_func() {
-
-    console.log("banner 2");
-
     banner_1.style.display = "none";
     banner_2.style.display = "block";
     banner_3.style.display = "none";
 
     setTimeout(banner_3_func, 5000);
-
 }
 
 function banner_3_func() {
-
-    console.log("banner 3");
-
     banner_1.style.display = "none";
     banner_2.style.display = "none";
     banner_3.style.display = "block";
--- singleviewJavascript.js
+++ singleviewJavascript.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", init);
 
 let urlParams = new URLSearchParams(window.location.search);
+console.log(urlParams);
 let id = urlParams.get("id");
 
 let singleurl;
@@ -15,18 +16,14 @@ let banner_1 = document.querySelector(".banner_1");
 let banner_2 = document.querySelector(".banner_2");
 let banner_3 = document.querySelector(".banner_3");
 
-
 function init() {
-
     getJSON();
     banner_1_func();
-
 }
 
 async function getJSON() {
-
-
-    singleurl = "https://www.rebeccamortensen.dk/kea/lasaite/wp-json/wp/v2/products";
+    singleurl =
+        "https://www.rebeccamortensen.dk/kea/lasaite/wp-json/wp/v2/products";
 
     console.log(id);
 
@@ -35,16 +32,12 @@ async function getJSON() {
     single = await jsonData.json();
 
     visProducts();
-
 }
 
 function visProducts() {
-
-
     postContainer.innerHTML = "";
 
     console.log(single);
-
 
     document.querySelector("[data-price]").textContent = single.acf.price;
     document.querySelector("[data-title]").textContent = single.acf.name;
@@ -52,13 +45,7 @@ function visProducts() {
     document.querySelector("[data-colour]").textContent = single.acf.colour;
     document.querySelector("[data-size]").textContent = single.acf.size;
     document.querySelector("[data-desc]").textContent = single.acf.description;
-
-
-
-
-
 }
-
 
 function myDropMenu() {
     document.getElementById("myDropdown").classList.toggle("show");
@@ -67,21 +54,19 @@ function myDropMenu() {
 // Close the dropdown menu if the user clicks outside of it
 
 window.onclick = function (event) {
-    if (!event.target.matches('.dropbtn')) {
-
+    if (!event.target.matches(".dropbtn")) {
         let dropdowns = document.getElementsByClassName("dropdown-content");
         let i;
         for (i = 0; i < dropdowns.length; i++) {
             let openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
+            if (openDropdown.classList.contains("show")) {
+                openDropdown.classList.remove("show");
             }
         }
     }
-}
+};
 
 function banner_1_func() {
-
     console.log("banner 1");
 
     banner_1.style.display = "block";
@@ -89,11 +74,9 @@ function banner_1_func() {
     banner_3.style.display = "none";
 
     setTimeout(banner_2_func, 5000);
-
 }
 
 function banner_2_func() {
-
     console.log("banner 2");
 
     banner_1.style.display = "none";
@@ -101,11 +84,9 @@ function banner_2_func() {
     banner_3.style.display = "none";
 
     setTimeout(banner_3_func, 5000);
-
 }
 
 function banner_3_func() {
-
     console.log("banner 3");
 
     banner_1.style.display = "none";
